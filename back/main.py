5d55faea7a2ef7a758fd9633f876a94eee80a246
SerejS <sokolovserej@gmail.com>
2023-04-27 21:54:34 +0300

    fix getting id

--- back/main.py
+++ back/main.py
@@ -34,23 +34,22 @@ def get_poll_answers(poll_id, owner_id):
             f"name_case=nom&v=5.131&" \
             f"access_token={token}"
 
-    print(query)
     response_json = requests.get(query).json()
     answers_json = response_json["response"]["answers"]
 
     answers = {}
     for answer in answers_json:
         answers[answer['id']] = answer['text']
-    lessons = get_vote_results(poll_id, answers)
+    lessons = get_vote_results(poll_id, owner_id, answers)
 
     return lessons
 
 
-def get_vote_results(poll_id, answers):
+def get_vote_results(poll_id, owner_id, answers):
     answer_id_strs = ','.join(map(lambda key: str(key), answers.keys()))
 
     query = f"{base_url}/polls.getVoters?" \
-            f"owner_id=736068632&" \
+            f"owner_id={owner_id}&" \
             f"poll_id={poll_id}&" \
             f"name_case=nom&v=5.131&" \
             f"answer_ids={answer_id_strs}&" \
@@ -65,22 +64,15 @@ def get_vote_results(poll_id, answers):
         lessons.append(lesson)
     return lessons
 
-# gets from poll url owner and poll ids
-def data_from_url():
-    pass
-
 
 app = FastAPI()
 app.mount('/static',
           StaticFiles(directory='../front/build/static', html=True), name='static')
 
-origins = [
-    "http://localhost:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
